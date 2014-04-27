b83495d04cf25f1eaf0304df03c073d65f5bc390
Oliver Gafvert <olivergafvert@gmail.com>
2014-04-27 23:41:22 +0200

    Fixed tiny spelling error

--- src/index/context/ContextIndexer.java
+++ src/index/context/ContextIndexer.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public interface ContextIndexer {
 	
-	void bildIndex(Index index, File indexDirectory);
+	void buildIndex(Index index, File indexDirectory);
 
     //void addContextScore (String firstWord, String secondWord, double score);
 
