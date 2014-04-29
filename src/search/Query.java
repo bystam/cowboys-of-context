26e76ac59e22bdfe3de7befea2027329a1988142
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-04-29 11:26:40 +0200

    comments out non-compiling class for now

--- src/search/Query.java
+++ src/search/Query.java
@@ -1,34 +1,26 @@
 package search;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 
 public class Query implements Iterable<Entry<String,Double>> {
 	
 	public static final double DEFAULT_WEIGHT = 1;
 
-	private final Map<String, Double> weightedTerms;
+	private final Map<String, Double> weightedTerms = new HashMap<>();
 
     public Query (String queryString) {
         queryString = queryString.trim();
         List<String> terms = Arrays.asList(queryString.split(" \t"));
-        weightedTerms = new HashMap<String,Double>();
-        for(String word : terms){
+        for (String word : terms)
         	weightedTerms.put(word, DEFAULT_WEIGHT);
-        }
     }
     
     public void addOrIncrementTermWeight(String term, double weight){
-    	if(weightedTerms.containsKey(term)){
+    	if (weightedTerms.containsKey(term)) {
     		weightedTerms.put(term, weightedTerms.get(term) + weight);
-    	}else{
+    	} else {
     		weightedTerms.put(term,  weight);
     	}
     }
