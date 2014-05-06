57180a1c492336367f4b83b5dbecdaebec5307c0
JonathanMurray <jonte_murray@hotmail.com>
2014-05-06 16:39:38 +0200

    added composite filter and blacklist filter

--- src/index/context/BlackListContextFilter.java
+++ src/index/context/BlackListContextFilter.java
@@ -1,11 +1,136 @@
 package index.context;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class BlackListContextFilter implements ContextFilter {
+	
+	
 
 	private final Set<String> blacklist = new HashSet<String>();
+	
+	public BlackListContextFilter(){
+		blacklist.addAll(Arrays.asList(new String[]{
+				"alla",
+				"allt", 
+				"alltså", 
+				"andra", 
+				"att", 
+				"bara", 
+				"bli", 
+				"blir", 
+				"borde", 
+				"bra", 
+				"mitt", 
+				"ser", 
+				"dem", 
+				"den", 
+				"denna", 
+				"det", 
+				"detta", 
+				"dig", 
+				"din", 
+				"dock", 
+				"dom", 
+				"där", 
+				"edit", 
+				"efter", 
+				"eftersom", 
+				"eller", 
+				"ett", 
+				"fast", 
+				"fel", 
+				"fick", 
+				"finns", 
+				"fram", 
+				"från", 
+				"får", 
+				"fått", 
+				"för", 
+				"första", 
+				"genom", 
+				"ger", 
+				"går", 
+				"gör", 
+				"göra", 
+				"hade", 
+				"han", 
+				"har", 
+				"hela", 
+				"helt", 
+				"honom", 
+				"hur", 
+				"här", 
+				"i",
+				"iaf", 
+				"igen", 
+				"ingen", 
+				"inget", 
+				"inte", 
+				"jag", 
+				"kan", 
+				"kanske", 
+				"kommer", 
+				"lika", 
+				"lite", 
+				"man", 
+				"med", 
+				"men", 
+				"mer", 
+				"mig", 
+				"min", 
+				"mot", 
+				"mycket", 
+				"många", 
+				"måste", 
+				"nog", 
+				"när", 
+				"någon", 
+				"något", 
+				"några", 
+				"nån", 
+				"nåt", 
+				"och", 
+				"också", 
+				"rätt", 
+				"samma", 
+				"sedan", 
+				"sen", 
+				"sig", 
+				"sin", 
+				"själv", 
+				"ska", 
+				"skulle", 
+				"som", 
+				"sätt", 
+				"tar", 
+				"till", 
+				"tror", 
+				"tycker", 
+				"typ", 
+				"upp", 
+				"utan", 
+				"vad", 
+				"var", 
+				"vara", 
+				"vet", 
+				"vid", 
+				"vilket", 
+				"vill", 
+				"väl", 
+				"även", 
+				"över" 
+		}));
+		
+	}
+	
 	
 	/* BlackListContextFilter.isValid is used to check if a word
 	 * is important enough to be considered in the
